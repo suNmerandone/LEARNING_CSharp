9f0857ea5f5bfdab3ff31de841ad68582b96667f
su'N'mer <6932kenzooii@gmail.com>
2019-10-08 21:36:11 +0800

    Turn class/unit test into partial class so that could has better management.

---
+++ LCConsoleAppTests/LCProgramTests.MincostTickets.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LCConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit.Sdk;
+
+namespace LCConsoleApp.Tests
+{
+    public partial class LCProgramTests
+    {
+        [TestClass()]
+        public class LCTests
+        {
+            [TestMethod()]
+            public void MincostTicketsTest_Return11()
+            {
+                // arrange
+                var LCSolution = new LCProgram();
+                var expected = 11;
+
+                // act
+                int[] days = new int[6] { 1, 4, 6, 7, 8, 20 };
+                int[] costs = new int[3] { 2, 7, 15 };
+                var actual = LCSolution.MincostTickets(days, costs);
+
+                // assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod()]
+            public void MincostTicketsTest_Return17()
+            {
+                // arrange
+                var LCSolution = new LCProgram();
+                var expected = 17;
+
+                // act
+                int[] days = new int[12] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31 };
+                int[] costs = new int[3] { 2, 7, 15 };
+                var actual = LCSolution.MincostTickets(days, costs);
+
+                // assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod()]
+            public void MincostTicketsTest_Return6()
+            {
+                // arrange
+                var LCSolution = new LCProgram();
+                var expected = 6;
+
+                // act
+                int[] days = new int[12] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31 };
+                int[] costs = new int[3] { 7, 2, 15 };
+                var actual = LCSolution.MincostTickets(days, costs);
+
+                // assert
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
--- LCConsoleAppTests/LCProgramTests.cs
+++ LCConsoleAppTests/LCProgramTests.cs
@@ -8,59 +8,8 @@ using Xunit.Sdk;
 namespace LCConsoleApp.Tests
 {
     [TestClass()]
-    public class LCProgramTests
+    public partial class LCProgramTests
     {
-        [TestClass()]
-        public class LCTests
-        {
-            [TestMethod()]
-            public void MincostTicketsTest_Return11()
-            {
-                // arrange
-                var LCSolution = new LCProgram();
-                var expected = 11;
-
-                // act
-                int[] days = new int[6] { 1, 4, 6, 7, 8, 20 };
-                int[] costs = new int[3] { 2, 7, 15 };
-                var actual = LCSolution.MincostTickets(days, costs);
-
-                // assert
-                Assert.AreEqual(expected, actual);
-            }
-
-            [TestMethod()]
-            public void MincostTicketsTest_Return17()
-            {
-                // arrange
-                var LCSolution = new LCProgram();
-                var expected = 17;
-
-                // act
-                int[] days = new int[12] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31 };
-                int[] costs = new int[3] { 2, 7, 15 };
-                var actual = LCSolution.MincostTickets(days, costs);
-
-                // assert
-                Assert.AreEqual(expected, actual);
-            }
-
-            [TestMethod()]
-            public void MincostTicketsTest_Return6()
-            {
-                // arrange
-                var LCSolution = new LCProgram();
-                var expected = 6;
-
-                // act
-                int[] days = new int[12] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 30, 31 };
-                int[] costs = new int[3] { 7, 2, 15 };
-                var actual = LCSolution.MincostTickets(days, costs);
-
-                // assert
-                Assert.AreEqual(expected, actual);
-            }
-        }
     }
 }
 

0f97949948b2cbe4ee32499afc7429f86127e342
su'N'mer <6932kenzooii@gmail.com>
2021-07-13 17:30:52 +0800

    Implemented 1 problem of LeetCode.

---
+++ LCConsoleApp/LCProgram.977.SquaresSortedArray.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LCConsoleApp
+{
+    public partial class LCProgram
+    {
+        /*
+         * Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+         */
+
+        public int[] SortedSquares(int[] nums)
+        {
+            int[] retArray = new int[] { };
+
+            List<int> numList = new List<int>();
+            foreach (int num in nums)
+            {
+                int squNum = (int)Math.Pow(num, 2);
+                numList.Add(squNum);
+            }
+
+            numList.Sort();
+            retArray = numList.ToArray();
+            return retArray;
+        }
+
+        /*
+         * two-pointer solution
+            Time: O(n) where n is the size of input array
+            Space: O(n) due to the output array
+        */
+        //public int[] SortedSquares(int[] A)
+        //{
+
+        //    int[] res = new int[A.Length];
+        //    int left = 0, right = A.Length - 1;
+
+        //    for (int i = A.Length - 1; i >= 0; i--)
+        //    {
+        //        if (Math.Abs(A[left]) >= Math.Abs(A[right]))
+        //        {
+        //            res[i] = A[left] * A[left];
+        //            left++;
+        //        }
+        //        else
+        //        {
+        //            res[i] = A[right] * A[right];
+        //            right--;
+        //        }
+        //    }
+
+        //    return res;
+        //}
+    }
+}
---
+++ LCConsoleAppTests/LCProgramTests.977.SquaresSortedArray.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LCConsoleApp.Tests
+{
+    public partial class LCProgramTests
+    {
+        public partial class LCTests
+        {
+            [TestMethod()]
+            public void SortedSquares_410310Return01916100()
+            {
+                // arrange
+                var LCSolution = new LCProgram();
+                int[] outArray = new int[] { 0, 1, 9, 16, 100 };
+                var expected = true;
+
+                // act
+                int[] inArray = new int[] { -4, -1, 0, 3, 10 };
+                int[] actualArray = LCSolution.SortedSquares(inArray);
+                var actual = System.Linq.Enumerable.SequenceEqual(outArray, actualArray);
+
+                // assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod()]
+            public void SortedSquares_732311Return49949121()
+            {
+                // arrange
+                var LCSolution = new LCProgram();
+                int[] outArray = new int[] { 4, 9, 9, 49, 121 };
+                var expected = true;
+
+                // act
+                int[] inArray = new int[] { -7, -3, 2, 3, 11 };
+                int[] actualArray = LCSolution.SortedSquares(inArray);
+                var actual = System.Linq.Enumerable.SequenceEqual(outArray, actualArray);
+
+                // assert
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}

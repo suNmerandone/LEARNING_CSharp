4dfd2c61c4469aa0c15a5eff1fea8f0d665079a6
su'N'mer <6932kenzooii@gmail.com>
2021-12-11 17:23:30 +0800

    Implemented LeetCode question - Delete node in a ListNode

---
+++ LCConsoleApp/Top Interview Questions - Easy Collection/LCProgram.LinkedList.DeleteNode..cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LCConsoleApp
+{
+    public partial class LCProgram
+    {
+        /*
+         * Write a function to delete a node in a singly-linked list. 
+         * You will not be given access to the head of the list, instead you will be given access to the node to be deleted directly.
+         * It is guaranteed that the node to be deleted is not a tail node in the list.
+         */
+
+        public class ListNode_TIQ_DelNode
+        {
+            public int val;
+            public ListNode_TIQ_DelNode next;
+            public ListNode_TIQ_DelNode(int x)
+            {
+                val = x;
+                //next = null;
+            }
+        }
+
+        public void DeleteNode(ListNode_TIQ_DelNode node)
+        {
+            ListNode_TIQ_DelNode currentNode = node;
+            ListNode_TIQ_DelNode nextNode = node.next;
+
+            currentNode.val = nextNode.val;
+            currentNode.next = nextNode.next;
+        }
+    }
+}
---
+++ LCConsoleAppTests/Top Interview Questions - Easy Collection/LCProgramTest.LinkedList.DeleteNode..cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LCConsoleApp.Tests
+{
+    public partial class LCProgramTests
+    {
+        public partial class LCTests
+        {
+            [TestMethod()]
+            public void DeleteNodeTest_4519Del5Return419True()
+            {
+                // arrange
+                var LCSolution = new LCProgram();
+                var expected = true;
+
+                // act
+                LCProgram.ListNode_TIQ_DelNode node_0 = new LCProgram.ListNode_TIQ_DelNode(4);
+                LCProgram.ListNode_TIQ_DelNode node_1 = new LCProgram.ListNode_TIQ_DelNode(5);
+                LCProgram.ListNode_TIQ_DelNode node_2 = new LCProgram.ListNode_TIQ_DelNode(1);
+                LCProgram.ListNode_TIQ_DelNode node_3 = new LCProgram.ListNode_TIQ_DelNode(9);
+
+                node_0.next = node_1;
+                node_1.next = node_2;
+                node_2.next = node_3;
+                node_3.next = null;
+
+                LCSolution.DeleteNode(node_1);
+
+                string strAfter = string.Empty;
+                var curNode = node_0;
+                while (curNode.next != null)
+                {
+                    strAfter += curNode.val.ToString();
+                    curNode = curNode.next;
+                }
+                strAfter += curNode.val.ToString();         // Last node
+
+                bool actual = false;
+                if (strAfter.CompareTo("419") == 0)
+                {
+                    actual = true;
+                }
+
+                // assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod()]
+            public void DeleteNodeTest_4519Del1Return459True()
+            {
+                // arrange
+                var LCSolution = new LCProgram();
+                var expected = true;
+
+                // act
+                LCProgram.ListNode_TIQ_DelNode node_0 = new LCProgram.ListNode_TIQ_DelNode(4);
+                LCProgram.ListNode_TIQ_DelNode node_1 = new LCProgram.ListNode_TIQ_DelNode(5);
+                LCProgram.ListNode_TIQ_DelNode node_2 = new LCProgram.ListNode_TIQ_DelNode(1);
+                LCProgram.ListNode_TIQ_DelNode node_3 = new LCProgram.ListNode_TIQ_DelNode(9);
+
+                node_0.next = node_1;
+                node_1.next = node_2;
+                node_2.next = node_3;
+                node_3.next = null;
+
+                LCSolution.DeleteNode(node_2);
+
+                string strAfter = string.Empty;
+                var curNode = node_0;
+                while (curNode.next != null)
+                {
+                    strAfter += curNode.val.ToString();
+                    curNode = curNode.next;
+                }
+                strAfter += curNode.val.ToString();         // Last node
+
+                bool actual = false;
+                if (strAfter.CompareTo("459") == 0)
+                {
+                    actual = true;
+                }
+
+                // assert
+                Assert.AreEqual(expected, actual);
+            }
+
+            [TestMethod()]
+            public void DeleteNodeTest_neg3599DelNeg3ReturnNeg599True()
+            {
+                // arrange
+                var LCSolution = new LCProgram();
+                var expected = true;
+
+                // act
+                LCProgram.ListNode_TIQ_DelNode node_0 = new LCProgram.ListNode_TIQ_DelNode(-3);
+                LCProgram.ListNode_TIQ_DelNode node_1 = new LCProgram.ListNode_TIQ_DelNode(5);
+                LCProgram.ListNode_TIQ_DelNode node_2 = new LCProgram.ListNode_TIQ_DelNode(-99);
+
+                node_0.next = node_1;
+                node_1.next = node_2;
+                node_2.next = null;
+
+                LCSolution.DeleteNode(node_0);
+
+                string strAfter = string.Empty;
+                var curNode = node_0;
+                while (curNode.next != null)
+                {
+                    strAfter += curNode.val.ToString();
+                    curNode = curNode.next;
+                }
+                strAfter += curNode.val.ToString();         // Last node
+
+                bool actual = false;
+                if (strAfter.CompareTo("5-99") == 0)
+                {
+                    actual = true;
+                }
+
+                // assert
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
